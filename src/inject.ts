8a8102911cf847e610ba4b10431a257fa1b8a46a
普联数建-杨宏旋 <782042369@qq.com>
2023-11-01 15:13:12 +0800

    feat: 美化按钮样式。

--- src/inject.ts
+++ src/inject.ts
@@ -2,43 +2,107 @@
  * @Author: yanghongxuan
  * @Date: 2023-11-01 12:48:49
  * @LastEditors: yanghongxuan
- * @LastEditTime: 2023-11-01 14:25:02
+ * @LastEditTime: 2023-11-01 15:13:06
  * @Description:
  */
 import { getLedTorrent, getNPHPUserTorrent } from '@/api';
 
+const cssContent = `.bubbly-button {
+    font-family: "Helvetica", "Arial", sans-serif;
+    display: inline-block;
+    font-size: 20px;
+    padding: 8px 10px;
+    appearance: none;
+    background-color: #ff0081;
+    color: #fff;
+    border-radius: 4px;
+    border: none;
+    cursor: pointer;
+    position: relative;
+    transition: transform ease-in 0.1s, box-shadow ease-in 0.25s;
+    box-shadow: 0 2px 25px rgba(255, 0, 130, 0.5);
+    position: fixed;
+    top: 80px;
+    left: 20px;
+    zIndex: 9999;
+  }
+  .bubbly-button:focus {
+    outline: 0;
+  }
+  .bubbly-button:before, .bubbly-button:after {
+    position: absolute;
+    content: "";
+    display: block;
+    width: 140%;
+    height: 100%;
+    left: -20%;
+    z-index: -1000;
+    transition: all ease-in-out 0.5s;
+    background-repeat: no-repeat;
+  }
+  .bubbly-button:before {
+    display: none;
+    top: -75%;
+    background-image: radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, transparent 20%, #ff0081 20%, transparent 30%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, transparent 10%, #ff0081 15%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%);
+    background-size: 10% 10%, 20% 20%, 15% 15%, 20% 20%, 18% 18%, 10% 10%, 15% 15%, 10% 10%, 18% 18%;
+  }
+  .bubbly-button:after {
+    display: none;
+    bottom: -75%;
+    background-image: radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, transparent 10%, #ff0081 15%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%);
+    background-size: 15% 15%, 20% 20%, 18% 18%, 20% 20%, 15% 15%, 10% 10%, 20% 20%;
+  }
+  .bubbly-button:active {
+    transform: scale(0.9);
+    background-color: #e60074;
+    box-shadow: 0 2px 25px rgba(255, 0, 130, 0.2);
+  }
+  .bubbly-button.animate:before {
+    display: block;
+    animation: topBubbles ease-in-out 0.75s forwards;
+  }
+  .bubbly-button.animate:after {
+    display: block;
+    animation: bottomBubbles ease-in-out 0.75s forwards;
+  }
+
+  @keyframes topBubbles {
+    0% {
+      background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%, 40% 90%, 55% 90%, 70% 90%;
+    }
+    50% {
+      background-position: 0% 80%, 0% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%, 50% 50%, 65% 20%, 90% 30%;
+    }
+    100% {
+      background-position: 0% 70%, 0% 10%, 10% 30%, 20% -10%, 30% 20%, 22% 40%, 50% 40%, 65% 10%, 90% 20%;
+      background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+    }
+  }
+  @keyframes bottomBubbles {
+    0% {
+      background-position: 10% -10%, 30% 10%, 55% -10%, 70% -10%, 85% -10%, 70% -10%, 70% 0%;
+    }
+    50% {
+      background-position: 0% 80%, 20% 80%, 45% 60%, 60% 100%, 75% 70%, 95% 60%, 105% 0%;
+    }
+    100% {
+      background-position: 0% 90%, 20% 90%, 45% 70%, 60% 110%, 75% 80%, 95% 70%, 110% 10%;
+      background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+    }
+  }`;
 document.addEventListener('DOMContentLoaded', () => {
+  let loading = false;
   const button = document.createElement('button');
-  // Applying Styles
-  button.style.borderRadius = '8px';
-  button.style.border = '1px solid #555'; // Lighter border to start with
-  button.style.padding = '0.6em 1.2em';
-  button.style.fontSize = '30px';
-  button.style.fontWeight = '500';
-  button.style.fontFamily = 'inherit';
-  button.style.backgroundColor = '#1a1a1a';
-  button.style.color = '#FFF'; // Added white color for the text for better contrast
-  button.style.cursor = 'pointer';
-  button.style.position = 'fixed';
-  button.style.top = '20px';
-  button.style.left = '20px';
-  button.style.zIndex = '9999';
-  button.style.transition =
-    'background-color 0.25s, color 0.25s, border-color 0.25s'; // Extended transition
+  function includeCss() {
+    // 创建一个新的 <style> 元素
+    const styleEl = document.createElement('style');
+    styleEl.textContent = cssContent;
+    // 将 <style> 元素插入到文档的头部
+    document.head.appendChild(styleEl);
+  }
 
-  // Adding hover effect
-  button.addEventListener('mouseover', function () {
-    this.style.backgroundColor = '#333';
-    this.style.color = '#FFF';
-    this.style.borderColor = '#888';
-  });
-
-  button.addEventListener('mouseout', function () {
-    this.style.backgroundColor = '#1a1a1a';
-    this.style.color = '#FFF';
-    this.style.borderColor = '#555';
-  });
-
+  includeCss();
+  button.className = 'bubbly-button';
   const handleLedTorrent = async (
     arr: {
       id: number;
@@ -51,8 +115,10 @@ document.addEventListener('DOMContentLoaded', () => {
         button.innerText = `一键认领进度：${arr.length} / ${i + 0}`;
       }
       button.innerText = '一键认领完毕，刷新查看。';
+      loading = false;
     }
     console.error('查询用户领种信息异常');
+    loading = false;
   };
 
   /** 获取当前用户种子数据 */
@@ -81,6 +147,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const getUserTorrents = async () => {
     try {
       // /api/user-seeding-torrent
+      if (loading) {
+        return;
+      }
+      loading = true;
       button.innerText = '正在前台运行中不要退出！！！';
       let num = 1;
       const list = await getUserTorrent(num);
@@ -98,11 +168,30 @@ document.addEventListener('DOMContentLoaded', () => {
     } catch (error) {
       console.error('error: ', error);
       button.innerText = '查询用户领种信息异常';
+      loading = false;
     }
   };
-  button.addEventListener('click', function () {
-    getUserTorrents();
-  });
+  var animateButton = function (e: any) {
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function () {
+      e.target.classList.remove('animate');
+    }, 700);
+  };
+
+  button.addEventListener(
+    'click',
+    function (e) {
+      getUserTorrents();
+      animateButton(e);
+    },
+    false
+  );
   button.innerText = '一键认领';
-  document.body.appendChild(button);
+  if (location.href.includes('https://zmpt.cc/userdetails.php')) {
+    document.body.appendChild(button);
+  }
 });
